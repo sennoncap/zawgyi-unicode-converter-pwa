e9d0f2dd7cc661831c353bd54cef21965617b979
Moe Myat Zaw <mmzliveid@gmail.com>
2019-09-04 16:13:31 +0630

    Add deploy script file

---
+++ copy-files.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+// tslint:disable-next-line: no-var-requires no-require-imports no-unsafe-any
+const packageVersion = require('./package.json').version;
+
+const destDir = path.resolve(__dirname, './dist');
+
+// Copy release notes
+fs.copyFileSync(path.resolve(__dirname, `./release-notes/v${packageVersion}/README.md`), path.join(destDir, 'RELEASENOTES.md'));
+
+// Copy firebase configs
+fs.copyFileSync(path.resolve(__dirname, './firebase.json'), path.join(destDir, 'firebase.json'));
+fs.copyFileSync(path.resolve(__dirname, './.firebaserc'), path.join(destDir, '.firebaserc'));
