8f0650f2b18c6e5fc31d5f3a933b390ebcba10c4
Moe Myat Zaw <mmzliveid@gmail.com>
2019-10-11 13:10:28 +0630

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,23 +12,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppBrowserModule } from './app/app.browser.module';
 import { environment } from './environments/environment';
 
-// tslint:disable-next-line: no-any
-declare var gtag: any;
-
 if (environment.production) {
     enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppBrowserModule)
     .catch(err => {
-        // tslint:disable-next-line: no-typeof-undefined
-        if (environment.production && typeof window !== 'undefined' && gtag) {
-            // tslint:disable-next-line: no-unsafe-any
-            gtag('event', 'exception', {
-                description: `${err}`,
-                fatal: true
-            });
-        } else {
-            console.error(err);
-        }
+        console.error(err);
     });
