eca20fcf7b1b21e93347f8ed5ca031f4e92cba54
Moe Myat Zaw <mmzliveid@gmail.com>
2019-12-01 11:03:25 +0630

    Update appTitleFull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,7 +84,7 @@ export class AppComponent implements OnInit, OnDestroy {
     }
 
     get appTitleFull(): string {
-        return `${this.appTitle} - Intelligent Accurate Cross Platform App`;
+        return `${this.appTitle} | Myanmar Tools`;
     }
 
     get appVersion(): string {
