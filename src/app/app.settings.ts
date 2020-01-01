8554c5e7f612bab36b05d324e6da8b68db17e06e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-01 22:36:11 +0630

    Set v3.6.1

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,8 +2,8 @@ import { AppConfig } from './shared/app-config';
 
 export const appSettings: { app: AppConfig } = {
     app: {
-        appVersion: '3.6.0',
-        releaseDateUtc: '2020-01-01T15:00:00Z',
+        appVersion: '3.6.1',
+        releaseDateUtc: '2020-01-01T16:15:00Z',
         appName: 'Zawgyi Unicode Converter',
         appDescription: 'Zawgyi Unicode Converter is a free & open source Zawgyi to Unicode or Unicode to Zawgyi online / offline Myanmar font converter by DagonMetric Myanmar Tools.',
         baseUrl: 'https://zawgyi-unicode-converter.myanmartools.org/',
