979f7a92d7fa9907932f5c8da3057d82e0eeb8b4
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-02 23:12:52 +0630

    Update copy-files.js

--- copy-files.js
+++ copy-files.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
+/* eslint-env node */
 
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 
-// tslint:disable-next-line: no-var-requires no-require-imports no-unsafe-any
 const packageVersion = require('./package.json').version;
 
 const destDir = path.resolve(__dirname, './dist');
 
 // Copy release notes
-fs.copyFileSync(path.resolve(__dirname, `./release-notes/v${packageVersion}/README.md`), path.join(destDir, 'RELEASENOTES.md'));
+fs.copyFileSync(
+    path.resolve(__dirname, `./release-notes/v${packageVersion}/README.md`),
+    path.join(destDir, 'RELEASENOTES.md')
+);
 
 // Copy firebase configs
 fs.copyFileSync(path.resolve(__dirname, './firebase.json'), path.join(destDir, 'app/firebase.json'));
