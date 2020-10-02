f7882f9e4ef3f827ca74887348d41d07b9d883ae
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-02 19:45:06 +0630

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,29 +1,25 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-// tslint:disable-next-line: no-import-side-effect
 import 'zone.js/dist/zone-testing';
 
 import { getTestBed } from '@angular/core/testing';
 
-import {
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-// tslint:disable: no-any
-// tslint:disable-next-line: no-reserved-keywords
-declare const require: any;
+declare const require: {
+    context(
+        path: string,
+        deep?: boolean,
+        filter?: RegExp
+    ): {
+        keys(): string[];
+        <T>(id: string): T;
+    };
+};
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
-);
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 // Then we find all the tests.
-// tslint:disable-next-line: no-unsafe-any
 const context = require.context('./', true, /\.spec\.ts$/);
 
 // And load the modules.
-// tslint:disable-next-line: no-unsafe-any
 context.keys().map(context);
