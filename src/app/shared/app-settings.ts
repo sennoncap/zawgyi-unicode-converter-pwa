1062c2a362a99cd7bb3716b7bc6183b28231f2cc
Moe Myat Zaw <mmzliveid@gmail.com>
2020-10-08 22:07:32 +0630

    Update app-settings.ts

--- src/app/shared/app-settings.ts
+++ src/app/shared/app-settings.ts
@@ -1,8 +1,8 @@
 import { AppConfig } from './app-config';
 
 export const appSettings: AppConfig = {
-    appVersion: '4.0.0',
-    releaseDateUtc: '2020-10-08T13:00:00Z',
+    appVersion: '4.0.1',
+    releaseDateUtc: '2020-10-08T15:45:00Z',
     appName: 'Zawgyi Unicode Converter',
     appDescription:
         'Zawgyi Unicode Converter is a free & open source Zawgyi to Unicode or Unicode to Zawgyi online / offline Myanmar font converter by DagonMetric Myanmar Tools.',
